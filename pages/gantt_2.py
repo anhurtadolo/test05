9ef3ebc28be927d43c262cc1ca4d9b42169feff5
anhurtadolo <86801415+anhurtadolo@users.noreply.github.com>
2023-06-29 12:57:48 +0200

    Create gantt_2.py

---
+++ pages/gantt_2.py
@@ -0,0 +1,21 @@
+from st_aggrid import AgGrid
+import streamlit as st
+import pandas as pd 
+import numpy as np
+import plotly.express as px
+from  PIL import Image
+import io 
+
+
+def draw_model_gantt_ui();
+    
+    st.set_page_config(layout='wide') #Choose wide mode as the default setting
+    st.markdown(""" <style> .font {                                          
+    font-size:30px ; font-family: 'Cooper Black'; color: #FF9633;} 
+    </style> """, unsafe_allow_html=True)
+    st.markdown('<p class="font">Upload your project plan file and generate Gantt chart instantly</p>', unsafe_allow_html=True)
+
+
+
+
+execute()
