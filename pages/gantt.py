1286bf782a4424ca57988bf80d4d6c8cbff29b38
anhurtadolo <86801415+anhurtadolo@users.noreply.github.com>
2023-06-29 11:37:20 +0200

    gannt v5
    v5

--- pages/gantt.py
+++ pages/gantt.py
@@ -1,10 +1,15 @@
-import ifcopenshell
 import streamlit as st
 import plotly.express as px
 import pandas as pd
-
-
 import plotly.figure_factory as ff
+
+
+session = st.session_state
+
+def initialize_session_state():
+    session["DataFrame"] = None
+    session["Classes"] = []
+    session["IsDataFrameLoaded"] = False
 
 df = [dict(Task="Job-1", Start='2017-01-01', Finish='2017-02-02', Resource='Complete'),
       dict(Task="Job-1", Start='2017-02-15', Finish='2017-03-15', Resource='Incomplete'),
@@ -23,37 +28,4 @@ fig = ff.create_gantt(df, colors=colors, index_col='Resource', show_colorbar=Tru
                       group_tasks=True)
 fig.show()
 
-def main():      
-    st.set_page_config(
-        layout= "wide",
-        page_title="CR protoypingAH_MG",
-        page_icon="✍️",
-    )
-    st.title("Streamlit IFC")
-    st.markdown(
-    """ 
-    ###  📁 Click on Browse File in the Side Bar to start
-    """
-    )
-
-    ## Add File uploader to Side Bar Navigation
-    st.sidebar.header('Model Loader')
-    st.sidebar.file_uploader("Choose a file", type=['ifc'], key="uploaded_file", on_change=callback_upload)
-
-    ## Add File Name and Success Message
-    if "is_file_loaded" in session and session["is_file_loaded"]:
-        st.sidebar.success(f'Project successfuly loaded')
-        st.sidebar.write("🔃 You can reload a new file  ")
-        
-        col1, col2 = st.columns([2,1])
-        col1.subheader(f'Start Exploring "{get_project_name()}"')
-        col2.text_input("✏️ Change Project Name", key="project_name_input")
-        col2.button("✔️ Apply", key="change_project_name", on_click=change_project_name())
-
-    st.sidebar.write ("Hurtado Lopez, Andres & Gutierrez, Miguel")
-    st.write("")
-    st.sidebar.write("")
-
-if __name__ == "__main__":
-    session = st.session_state
-    main()
+execute()
